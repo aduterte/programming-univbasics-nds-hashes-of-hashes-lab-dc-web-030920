4f937d28adf776a162a6813876c4ac93fe728b15
Alessandro Duterte <alexduterte@gmail.com>
2020-01-29 02:15:00 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -54,7 +54,8 @@ hoh = {
           sub_category: {
             label: "Genus",
             sub_category: {
-              label: "Species"
+              label: "Species",
+              sub_category: nil
             }
           }
         }
